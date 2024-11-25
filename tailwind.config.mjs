b7cfa4e1c51a8316b016e52bf28902d7333c7431
Bruno Arueira <contato@brunoarueira.com>
2024-11-25 18:53:31 -0300

    fix: replace old postcss to astro recommended way

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
+
 const defaultTheme = require('tailwindcss/defaultTheme')
 
-module.exports = {
-  content: [
-    './src/**/**/*.js',
-  ],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       margin: {
