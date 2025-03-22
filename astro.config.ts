93845f2456ed23e2515346431764947678ce3eac
Bruno Arueira <contato@brunoarueira.com>
2025-03-22 15:38:34 -0300

    fix: improve syntax highlighting for code blocks

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import rehypeExternalLinks from 'rehype-external-links';
+import rehypePrism from 'rehype-prism-plus';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 
 import mdx from "@astrojs/mdx";
@@ -9,11 +10,14 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    shikiConfig: {
-      theme: 'nord',
-      wrap: true
-    },
+    syntaxHighlight: false,
     rehypePlugins: [
+        [
+          rehypePrism,
+	  {
+	    showLineNumbers: true
+	  }
+	],
 	[
 	  rehypeExternalLinks,
 	  {
