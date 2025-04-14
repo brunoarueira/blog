89f0c113c977957dbdada0c21f2bd7ffa50f3e54
Bruno Arueira <contato@brunoarueira.com>
2025-04-14 10:35:02 -0300

    feat: setup the basic for SEO

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://brunoarueira.com',
   markdown: {
     syntaxHighlight: false,
     rehypePlugins: [
