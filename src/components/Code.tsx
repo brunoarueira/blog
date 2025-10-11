735a0abcfc5588c36504201bb2943b03033dfca4
Bruno Arueira <contato@brunoarueira.com>
2025-10-11 16:18:24 -0300

    wip

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -26,10 +26,12 @@ const Code = ({ children, ...props }: { children: ComponentChildren }) => {
   };
 
   return (
-    <div className="relative group -mx-4 md:mx-[inherit] [&_pre]:px-8 [&_code]:max-w-full md:[&_pre]:px-6">
-      <pre {...props} ref={snippetRef}>{children}</pre>
+    <div className="relative group">
+      <pre {...props} ref={snippetRef}>
+        {children}
+      </pre>
       <button
-        className="text-sm text-gray-300 bg-gray-700 border rounded absolute top-4 right-4 px-2 opacity-0 group-hover:opacity-100 transition-opacity"
+        className="text-sm text-gray-300 bg-gray-700 border rounded absolute top-8 right-4 px-2 opacity-0 group-hover:opacity-100 transition-opacity"
         onClick={() => {
 	  if (timeoutRef.current) {
 	    return false;
